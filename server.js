b428b895a9dd5bfc91852de169c7212e432bd73f
Mateusz Janicki <mj4nicki@gmail.com>
2021-11-27 00:31:44 +0100

    mess

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ var express = require("express")
 var app = express()
 var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
+var hbs = require('express-handlebars');
+var path = require("path")
+
+app.set('views', path.join(__dirname, 'views'));         // ustalamy katalog views
+app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));   // domyślny layout, potem można go zmienić
+app.set('view engine', 'hbs');
 app.get("/", function (req, res) {
-    res.send("<h1>first app on heroku!</h1>")
+    res.render('login.hbs', { layout: null });
+})
+app.get("/upload", function (req, res) {
+    res.render('upload.hbs', { layout: "main.hbs" });
+})
+app.get("/filemanager", function (req, res) {
+    res.render('filemanager.hbs', { layout: "main.hbs" });
+})
+app.get("/info", function (req, res) {
+    res.render('info.hbs', { layout: "main.hbs" });
 })
 
+
 app.listen(PORT, function () {
-    console.log("start serwera na porcie " + PORT)
+    console.log("start serwera na porcie " + PORT )
 })
